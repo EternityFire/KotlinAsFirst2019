60721a9f79d4f3529d894e26670d8379043085b6
EternityFire <alieksandr.fiedotov.2014@mail.ru>
2019-10-03 23:32:16 +0400

    Fixes and new programs from 2 and 3 lessons

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -171,13 +171,13 @@ fun segmentLength(a: Int, b: Int, c: Int, d: Int): Int =
     if ((c > b) || (d < a)) -1
     else if ((a <= d) && (a >= c)) when {
         b > d -> d - a
-        (b < d) || (b == d) -> b - a
+        (b < d) || (b == d) || (a == c) -> b - a
         a == b -> 1
         else -> -1
     }
     else if ((c <= b) && (c >= a)) when {
         d > b -> b - c
-        (b > d) || (a == c) -> d - c
+        (b > d) || (a == c) || (b == d) -> d - c
         c == d -> 1
         else -> -1
     }

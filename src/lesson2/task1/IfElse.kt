a7c2835f390d505ae9c52200a4b19108d7e4c2aa
EternityFire <alieksandr.fiedotov.2014@mail.ru>
2019-10-03 21:44:34 +0400

    Fixes and new programs from 2 and 3 lessons

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -169,16 +169,15 @@ fun triangleKind(a: Double, b: Double, c: Double): Int =
  */
 fun segmentLength(a: Int, b: Int, c: Int, d: Int): Int =
     if ((c > b) || (d < a)) -1
-    else if ((a == c) && (b == d)) b - a
-    else if ((a < d) && (a > c)) when {
+    else if ((a <= d) && (a >= c)) when {
         b > d -> d - a
-        b < d -> b - a
+        (b < d) || (b == d) -> b - a
         a == b -> 1
         else -> -1
     }
-    else if ((c < b) && (c > a)) when {
+    else if ((c <= b) && (c >= a)) when {
         d > b -> b - c
-        b > d -> d - c
+        (b > d) || (a == c) -> d - c
         c == d -> 1
         else -> -1
     }
--- test/lesson2/task1/Tests.kt
+++ test/lesson2/task1/Tests.kt
@@ -80,8 +80,8 @@ class Tests {
     @Test
     @Tag("Normal")
     fun segmentLength() {
-        assertEquals(-1, segmentLength(1, 2, 3, 4))
-        assertEquals(-1, segmentLength(5, 7, 1, 3))
+        assertEquals(2, segmentLength(2, 4, 0, 4))
+        assertEquals(6, segmentLength(1, 7, 1, 7))
         assertEquals(0, segmentLength(1, 2, 2, 4))
         assertEquals(3, segmentLength(3, 6, 0, 9))
         assertEquals(2, segmentLength(2, 5, 3, 9))

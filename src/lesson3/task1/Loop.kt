bb13fce41fb28918a0abdd391b6c42f58db1bb41
EternityFire <alieksandr.fiedotov.2014@mail.ru>
2019-10-05 16:45:36 +0400

    Fixes of 2 and 3 lessons

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -163,7 +163,7 @@ fun isCoPrime(m: Int, n: Int): Boolean {
  * Например, для интервала 21..28 21 <= 5*5 <= 28, а для интервала 51..61 квадрата не существует.
  */
 fun squareBetweenExists(m: Int, n: Int): Boolean {
-    for (i in 0..sqrt(n.toDouble()).toInt())
+    for (i in sqrt(m.toDouble()).toInt()..sqrt(n.toDouble()).toInt())
         if ((i * i >= m) && (i * i <= n)) return true
     return false
 }

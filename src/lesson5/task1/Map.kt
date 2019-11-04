5a4bdb0707ce78f422d18b3ed4a355931d8ef32b
EternityFire <alieksandr.fiedotov.2014@mail.ru>
2019-11-04 16:48:44 +0400

    Fixes and some programs from 5th lesson

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -223,7 +223,11 @@ fun findCheapestStuff(stuff: Map<String, Pair<String, Double>>, kind: String): S
  * Например:
  *   canBuildFrom(listOf('a', 'b', 'o'), "baobab") -> true
  */
-fun canBuildFrom(chars: List<Char>, word: String): Boolean = TODO()
+fun canBuildFrom(chars: List<Char>, word: String): Boolean {
+    val s = word.toLowerCase().toSet()
+    val c = chars.toSet()
+    return c.containsAll(s)
+}
 
 /**
  * Средняя
